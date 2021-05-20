4986db8a95483cbc4d9aa6c0a8a05b3b7f6c567c
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-05-20 20:39:54 +0200

    Add SQL to identify the most popular items/categories

---
+++ publish-to-sns-lambda/lambda_handler.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+import os
+import base64
+
+sns_arn = os.environ["SNS_ARN"]
+
+sns = boto3.client("sns")
+
+def lambda_handler(event, context):
+    views_count = json.loads(
+        base64.b64decode(event["records"][0]["data"]))["TOP_10_VIEWS_COUNT"]
+        
+    sns.publish(
+        TargetArn=sns_arn,
+        Message=json.dumps({"default": f"Too many views for top 10 items: {views_count}"}),
+        MessageStructure='json'
+    )
+    
+    return {
+        "records": [
+            {"recordId": r["recordId"], "result": "Ok"} for r in event["records"]
+        ]
+    }
