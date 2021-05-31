9588bbe33c7ec1358240c444778ec281e580fd1e
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-06-01 00:57:18 +0200

    Get items in batches in ip-filter

--- ip-filter/lambda.py
+++ ip-filter/lambda.py
@@ -3,24 +3,40 @@ import base64
 import boto3
 import os
 
-dynamodb_client = boto3.client("dynamodb", region_name=os.environ["DYNAMO_REGION"])
+dynamodb_client = boto3.client(
+    "dynamodb", region_name=os.environ["DYNAMO_REGION"])
+
 
 def extract_user_ip(base64_data):
     return json.loads(base64.b64decode(base64_data))["user_ip"]
 
-def is_suspicious(user_ip):
-    response = dynamodb_client.get_item(
-        TableName=os.environ["TABLE_NAME"],
-        Key={"user_ip": {"S": user_ip}}
-    )
-    return "Item" in response
+
+def get_suspicious_ips(ips, batch_size=90):
+    table = os.environ["TABLE_NAME"]
+    suspicious_ips = set()
+    for i in range(0, len(ips), batch_size):
+        batch = ips[i:(i + batch_size)]
+        response = dynamodb_client.batch_get_item(
+            RequestItems={
+                f"{table}": {
+                    "Keys": [{"user_ip": {"S": user_ip}} for user_ip in ips]
+                }
+            }
+        )
+        suspicious_ips |= {r["user_ip"]["S"]
+                           for r in response["Responses"][f"{table}"]}
+    return suspicious_ips
+
 
 def lambda_handler(event, context):
+    ips = list({extract_user_ip(record["data"])
+                for record in event["records"]})
+    suspicious_ips = get_suspicious_ips(ips)
     return {
         "records": [
             {
                 "recordId": record["recordId"],
-                "result": "Dropped" if is_suspicious(extract_user_ip(record["data"])) else "Ok",
+                "result": "Dropped" if (extract_user_ip(record["data"]) in suspicious_ips) else "Ok",
                 "data": record["data"]
             } for record in event["records"]
         ]
